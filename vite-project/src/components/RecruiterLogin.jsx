b46982b6bc9dc7ee7a89cad554650790f9c7c849
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-01-13 20:00:24 +0300

    .

---
+++ vite-project/src/components/RecruiterLogin.jsx
@@ -0,0 +1,153 @@
+import React, { useContext, useEffect, useState } from "react";
+import { assets } from "../assets/assets";
+import { AppContext } from "../context/AppContext";
+
+const RecruiterLogin = () => {
+  const [state, setState] = useState("Giriş");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+
+  const [image, setImage] = useState(false);
+
+  const [isTextDataSubmited, setIsTextDataSubmited] = useState(false);
+
+  const {setShowRecruiterLogin} = useContext(AppContext)
+
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+
+    if (state == "Kayıt Ol" && !isTextDataSubmited) {
+      setIsTextDataSubmited(true);
+    }
+  };
+
+  useEffect(() =>{
+   document.body.style.overflow = 'hidden'
+
+   return () => {
+     document.body.style.overflow = 'unset'
+   }
+  },[])
+
+  return (
+    <div className="absolute top-0 left-0 right-0 bottom-0 z-10 backdrop-blur-sm bg-black/30 flex justify-center items-center">
+      <form
+        onSubmit={onSubmitHandler}
+        className="relative bg-white p-10 rounded-xl text-slate-500"
+      >
+        <h1 className="text-center text-2xl text-neutral-700 font-medium">
+          İşe Alımcı {state}{" "}
+        </h1>
+        <p className="text-sm ">
+          Tekrar hoş geldiniz! Devam etmek için lütfen giriş yapın.
+        </p>
+        {state === "Kayıt Ol" && isTextDataSubmited ? (
+          <>
+            <div className="flex items-center gap-4 my-10">
+              <label htmlFor="image">
+                <img
+                  className="w-16 rounded-full"
+                  src={image ? URL.createObjectURL(image) : assets.upload_area}
+                  alt=""
+                />
+                <input
+                  onChange={(e) => setImage(e.target.files[0])}
+                  type="file"
+                  id="image"
+                  hidden
+                />
+              </label>
+              <p>
+                Şirket Logosunu <br /> yükle{" "}
+              </p>
+            </div>
+          </>
+        ) : (
+          <>
+            {state !== "Giriş" && (
+              <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+                <img src={assets.person_icon} alt="" />
+                <input
+                  className="outline-none text-sm"
+                  onChange={(e) => setName(e.target.value)}
+                  value={name}
+                  type="text"
+                  placeholder="Şirket Adı"
+                  required
+                />
+              </div>
+            )}
+
+            <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+              <img src={assets.email_icon} alt="" />
+              <input
+                className="outline-none text-sm"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                placeholder="Şirket Email Adresi"
+                required
+              />
+            </div>
+
+            <div className="border px-4 py-2 flex items-center gap-2 rounded-full mt-5">
+              <img src={assets.lock_icon} alt="" />
+              <input
+                className="outline-none text-sm"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="Şifre"
+                required
+              />
+            </div>
+          </>
+        )}
+        {state === "Giriş" && (
+          <p className="text-wm text-blue-600 my-4 cursor-pointer">
+            Parolanızı mı unuttunuz ?
+          </p>
+        )}
+
+        <button
+          type="submit"
+          className="bg-blue-600 w-full text-white py-2 rounded-full mt-5 cursor-pointer"
+        >
+          {state === "Giriş"
+            ? "Giriş"
+            : isTextDataSubmited
+            ? "Hesap Oluştur"
+            : "Devam Et"}
+        </button>
+
+        {state === "Giriş" ? (
+          <p className="mt-5 text-center">
+            Hesabınız yok mu ?{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Kayıt Ol")}
+            >
+              Kayıt Ol
+            </span>
+          </p>
+        ) : (
+          <p className="mt-5 text-center">
+            Zaten hesabınız var mı?{" "}
+            <span
+              className="text-blue-600 cursor-pointer"
+              onClick={() => setState("Giriş")}
+            >
+              Giriş Yap
+            </span>
+          </p>
+        )}
+
+        <img onClick={e => setShowRecruiterLogin(false)} className="absolute top-5 right-5 cursor-pointer" src={assets.cross_icon} alt="" />
+
+      </form>
+    </div>
+  );
+};
+
+export default RecruiterLogin;
--- vite-project/src/context/AppContext.jsx
+++ vite-project/src/context/AppContext.jsx
@@ -15,6 +15,8 @@ export const AppContextProvider = (props) => {
 
  const [jobs, setJobs] = useState([])
 
+ const [showRecruiterLogin, setShowRecruiterLogin] = useState(false)
+
  // İş ilanlarını getirme işlevi
  const fetchJobs = async () => {
   setJobs(jobsData)
@@ -29,6 +31,7 @@ export const AppContextProvider = (props) => {
   setSearchFilter,searchFilter,
   isSearched, setIsSearched,
   jobs, setJobs,
+  showRecruiterLogin, setShowRecruiterLogin,
  }
  
  return(<AppContext.Provider value={value}>
