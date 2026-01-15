c3567bd013ad3bc818ed3a93a3a4af1f9807c0b1
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-01-15 18:29:55 +0300

    .

--- vite-project/src/pages/Dashboard.jsx
+++ vite-project/src/pages/Dashboard.jsx
@@ -1,9 +1,86 @@
 import React from 'react'
+import { NavLink, Outlet, useNavigate } from 'react-router-dom'
+import { assets } from '../assets/assets'
 
 const Dashboard = () => {
+
+  const navigate = useNavigate()
+
   return (
-    <div>
-      
+    <div className="min-h-screen flex flex-col">
+
+      {/* Navbar */}
+      <div className='shadow py-4'>
+        <div className='px-5 flex justify-between items-center'>
+          <img
+            onClick={() => navigate('/')}
+            className='max-sm:w-32 cursor-pointer'
+            src={assets.logo1}
+            alt=""
+          />
+
+          <div className='flex items-center gap-3'>
+            <p className='max-sm:hidden'>Hoşgeldin Oğuzhan</p>
+
+            <div className='relative group'>
+              <img
+                className='w-8 border border-gray-100 rounded-full'
+                src={assets.company_icon}
+                alt=""
+              />
+
+              <div className='absolute hidden group-hover:block top-0 right-0 z-10 rounded pt-12'>
+                <ul className='bg-white border  border-gray-100 rounded-md text-sm'>
+                  <li className='py-1 px-3 cursor-pointer'>Çıkış</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* CONTENT */}
+      <div className="flex items-start">
+
+        {/* Sol Panel */}
+        <div className="inline-block min-h-screen border-r-2 border-gray-200">
+          <ul className="flex flex-col items-start pt-5 text-gray-800">
+
+           
+              <NavLink
+                to="/dashboard/add-job"
+                className={({isActive}) => `flex items-center p-3 sm:px-6 gap-2 w-full hover:text-blue-600 ${isActive && 'bg-blue-100 border-r-2 border-blue-500'}`}
+              >
+                <img src={assets.add_icon} alt="" />
+                <p>İş Ekle</p>
+              </NavLink>
+        
+
+           
+              <NavLink
+                to="/dashboard/manage-job"
+                 className={({isActive}) => `flex items-center p-3 sm:px-6 gap-2 w-full hover:text-blue-600 ${isActive && 'bg-blue-100 border-r-2 border-blue-500'}`}
+              >
+                <img src={assets.home_icon} alt="" />
+                <p>İşleri Yönet</p>
+              </NavLink>
+         
+
+         
+              <NavLink
+                to="/dashboard/view-applications"
+                 className={({isActive}) => `flex items-center p-3 sm:px-6 gap-2 w-full hover:text-blue-600 ${isActive && 'bg-blue-100 border-r-2 border-blue-500'}`}
+              >
+                <img src={assets.person_tick_icon} alt="" />
+                <p>Uygulamaları Görüntüle</p>
+              </NavLink>
+ 
+          </ul>
+        </div>
+        <div>
+          <Outlet/>
+        </div>
+      </div>
     </div>
   )
 }
