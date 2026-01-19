d0bb648d7e21fa03b8f95c1bc893e658a445217d
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-01-19 17:35:11 +0300

    .

---
+++ server/controllers/companyController.js
@@ -0,0 +1,54 @@
+
+// Yeni bir şirket kaydedin
+const registerCompany = async(req,res) =>{
+
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+// Şirket Giriş
+export const loginCompany = async () =>{
+
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+// Şirket verilerini alın
+export const getCompanyData = async (req,res) =>{
+
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+// Yeni İş İlanı
+export const postJob = async (req,res) =>{
+
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+// Şirket iş başvurularını alın
+export const getCompanyJobApplicants = async (req,res) => {
+
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+// Şirket tarafından yayınlanan işleri alın
+export const getCompanyPostedJobs = async (req,res) => {
+
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+// İş başvurusu durumunu değiştir
+export const ChangeJobApplicationsStatus = async (req,res) => {
+
+}
+
+// --------------------------------------------------------------------------------------------------------
+
+// İş görünürlüğünü değiştirme
+export const ChangeVisiblity = async (req,res) => {
+
+}
