5bd99d8072857aae9c533f7331778de3479fd0ff
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-01-25 15:59:42 +0300

    .

--- vite-project/src/context/AppContext.jsx
+++ vite-project/src/context/AppContext.jsx
@@ -1,10 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 import { jobsData } from "../assets/assets";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 
 export const AppContext = createContext()
 
 export const AppContextProvider = (props) => {
+
+  const backendUrl = import.meta.env.VITE_BACKEND_URL
 
  const [searchFilter,setSearchFilter] = useState({
   title: '',
@@ -17,14 +21,53 @@ export const AppContextProvider = (props) => {
 
  const [showRecruiterLogin, setShowRecruiterLogin] = useState(false)
 
+ const [companyToken, setCompanyToken] = useState(null)
+ const [companyData, setCompanyData] = useState(null)
+
  // İş ilanlarını getirme işlevi
  const fetchJobs = async () => {
   setJobs(jobsData)
  }
 
+//  Function to fetch company data
+const fetchCompanyData = async () => {
+  try {
+    const { data } = await axios.get(
+      backendUrl + '/api/company/company',
+      {
+        headers: {
+          Authorization: `Bearer ${companyToken}`
+        }
+      }
+    )
+
+    if (data.success) {
+      setCompanyData(data.company)
+      console.log(data)
+    } else {
+      
+    }
+
+  } catch (error) {
+    toast.error(error.response?.data?.message || error.message)
+  }
+}
+
  useEffect(() => {
   fetchJobs();
+
+  const storedCompanyToken = localStorage.getItem('companyToken')
+
+  if (storedCompanyToken) {
+    setCompanyToken(storedCompanyToken)
+  }
 }, []);
+
+useEffect(()=>{
+  if (companyToken) {
+    fetchCompanyData()
+  }
+},[companyToken])
 
 
  const value = {
@@ -32,6 +75,9 @@ export const AppContextProvider = (props) => {
   isSearched, setIsSearched,
   jobs, setJobs,
   showRecruiterLogin, setShowRecruiterLogin,
+  companyToken,setCompanyToken,
+  companyData,setCompanyData,
+  backendUrl,
  }
  
  return(<AppContext.Provider value={value}>
